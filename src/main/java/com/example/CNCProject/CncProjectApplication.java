84b9519b2c0f96694a3e0457780d806cda792b44
Redle <redle220@gmail.com>
2024-04-10 19:58:18 +0300

    test_10_04

---
+++ src/main/java/com/example/CNCProject/CncProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.CNCProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CncProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CncProjectApplication.class, args);
+	}
+
+}
