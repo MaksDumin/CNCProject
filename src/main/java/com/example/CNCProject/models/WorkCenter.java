e511f63457127a1c88e0700df4cb2050f73e0360
WinHome <redle1995@mail.ru>
2024-04-11 21:44:26 +0300

    test 11_04_21_44

--- src/main/java/com/example/CNCProject/models/WorkCenter.java
+++ src/main/java/com/example/CNCProject/models/WorkCenter.java
@@ -2,12 +2,15 @@ package com.example.CNCProject.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class WorkCenter {
     private long id;
     private String title;
     private String code;
     private String plm;
 }
+
